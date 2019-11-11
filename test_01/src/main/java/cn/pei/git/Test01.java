ed241500f46134041373318add1973821b4b6535
classg <715163756Ing?>
2019-11-11 21:14:50 +0800

    这是我第二上传代码了！！！ 添加了一个输出语句

--- test_01/src/main/java/cn/pei/git/Test01.java
+++ test_01/src/main/java/cn/pei/git/Test01.java
@@ -7,6 +7,7 @@ public class Test01 {
         System.out.println("2");
         System.out.println("xixiix");
         System.out.println("3");
+        System.out.println("hahahhahahah");
         System.out.println("aaa");
     }
 }
