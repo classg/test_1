2983dc89a14f802853b0b94b985aad929951cb45
classg <715163756Ing?>
2019-11-11 21:12:27 +0800

    这是我第一次上传代码了！！！

---
+++ test_01/src/main/java/cn/pei/git/Test01.java
@@ -0,0 +1,12 @@
+package cn.pei.git;
+
+public class Test01 {
+
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("xixiix");
+        System.out.println("3");
+        System.out.println("aaa");
+    }
+}
